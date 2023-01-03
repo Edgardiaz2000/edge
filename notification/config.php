4f5c6fd248c0748c34f3f33d24b5bed6cd83811d
Edgardiaz2000 <100384933+Edgardiaz2000@users.noreply.github.com>
2023-01-03 18:05:26 -0400

    Add files via upload

---
+++ notification/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $fcm_server_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
+    $fcm_topic = 'your_single_radio_topic';
+
+    $onesignal_app_id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx';
+    
+    $onesignal_rest_api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
+?>
