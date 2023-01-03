6bb47065deeb93505978790f32f40498ba8bd356
Edgardiaz2000 <100384933+Edgardiaz2000@users.noreply.github.com>
2023-01-03 18:06:59 -0400

    Update config.php

--- notification/config.php
+++ notification/config.php
@@ -1,8 +1,8 @@
 <?php
 
-    $fcm_server_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    $fcm_server_key = 'BBO3aS35nkTqhFC14fek_FEAtoWJGEFr3IFGz26SW_IlGpmmLIOBLXs58gHYfzSfBx_NAs7ERcla0gwNOmbTGUg';
 
-    $fcm_topic = 'your_single_radio_topic';
+    $fcm_topic = 'la_trompeta_radio';
 
     $onesignal_app_id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx';
     
