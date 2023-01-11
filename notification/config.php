024ec7a8db8802a371ae07b9f1277b93a3f3981f
Edgardiaz2000 <100384933+Edgardiaz2000@users.noreply.github.com>
2023-01-11 13:57:41 -0400

    Update config.php

--- notification/config.php
+++ notification/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $fcm_server_key = 'BBO3aS35nkTqhFC14fek_FEAtoWJGEFr3IFGz26SW_IlGpmmLIOBLXs58gHYfzSfBx_NAs7ERcla0gwNOmbTGUg';
+    $fcm_server_key = 'BHhT_txqO3O0uyPcZ7qY3LO2f6N47gX5LJXWbdpDQqyJ0fSBJ-mQbvDX0qQnZexpNLUEhCy9eUmqLlLky72f6IE';
 
     $fcm_topic = 'la_trompeta_radio';
 
